94d897ad8233d9ce4e8bedc5b7cb0bb79164f223
André Luiz Ghiringhelli <31973990+andrepprado@users.noreply.github.com>
2026-09-10 21:15:25 -0300

    Revise filament costs and configuration settings
    Updated filament costs and configurations.

--- script.js
+++ script.js
@@ -36,27 +36,19 @@ const CONFIG = {
 // ==============================
 
 const filamentos = [
-    { nome: "VOOLT - PETG AZUL HF", custo: 79.48, cor: "#0077C8" },
-    { nome: "VOOLT - PETG LARANJA HF", custo: 79.48, cor: "#F97316" },
-        { nome: "VOOLT - PLA BEGE CAUCASIANO VELVET HIGH SPEED PREMIUM", custo: 89.37, cor: "#D6BC9B" },
-    { nome: "VOOLT - PLA BRANCO VELVET HIGH SPEED PREMIUM", custo: 89.37, cor: "#F8F8F6" },
-    { nome: "VOOLT - PLA PRETO VELVET HIGH SPEED PREMIUM", custo: 89.37, cor: "#181818" },
-    { nome: "VOOLT - PLA VERMELHO VELVET HIGH SPEED PREMIUM", custo: 89.37, cor: "#C62828" },
 
-    { nome: "SUNLU - PETG AZUL BRILHANTE", custo: 75.20, cor: "#005EB8" },
-    { nome: "SUNLU - PETG BRANCO BRILHANTE", custo: 75.20, cor: "#FAFAFA" },
-    { nome: "SUNLU - PETG CINZA BRILHANTE", custo: 75.20, cor: "#8B8D91" },
-
-    { nome: "SUNLU - PLA MARMORE BRANCO", custo: 120.99, cor: "#ECEAE4" },
-
+    { nome: "VOOLT - PLA BEGE CAUCASIANO VELVET HIGH SPEED PREMIUM", custo: 92.32, cor: "#D6BC9B" },
+    { nome: "VOOLT - PLA BRANCO VELVET HIGH SPEED PREMIUM", custo: 92.32, cor: "#F8F8F6" },
+    { nome: "VOOLT - PLA PRETO VELVET HIGH SPEED PREMIUM", custo: 92.32, cor: "#181818" },
+    { nome: "VOOLT - PLA VERMELHO VELVET HIGH SPEED PREMIUM", custo: 92.32, cor: "#C62828" },
     { nome: "BAMBU LAB - PLA AMARELO", custo: 128.90, cor: "#FFD400" },
     { nome: "BAMBU LAB - PLA AZUL", custo: 128.90, cor: "#0057B8" },
     { nome: "BAMBU LAB - PLA VERDE", custo: 128.90, cor: "#00A651" },
-
+    { nome: "SUNLU - PETG AZUL BRILHANTE", custo: 75.20, cor: "#005EB8" },
+    { nome: "SUNLU - PLA MARMORE BRANCO", custo: 120.99, cor: "#ECEAE4" },
     { nome: "ELEGOO - PLA SILK ROXO E PRETO", custo: 113.90, cor: "#5B2A86" },
     { nome: "ELEGOO - PLA MATTE LARANJA", custo: 103.80, cor: "#E67E22" },
     { nome: "ELEGOO - PLA SILK ROXO E AZUL", custo: 113.90, cor: "#5D3FD3" },
-    { nome: "ELEGOO - PLA PRETO", custo: 102.75, cor: "#111111" },
 
     { nome: "MASTERPRINT - PETG MARROM", custo: 83.90, cor: "#6B4423" },
     { nome: "MASTERPRINT - PETG BEGE", custo: 83.90, cor: "#D9C2A0" }
